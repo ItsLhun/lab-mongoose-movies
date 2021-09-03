342ab39210b37aeaf08509127817fbdb8516aa0c
itslhun <nahuelghastin@outlook.com>
2021-09-03 16:56:43 +0200

    complete iteration4

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -12,6 +12,23 @@ celebsRouter.get('/', (req, res, next) => {
     });
 });
 
+celebsRouter.post('/', (req, res, next) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  const catchphrase = req.body.catchphrase;
+  Celebrity.create({ name, occupation, catchphrase })
+    .then((celebrities) => {
+      res.redirect('celebrities');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+celebsRouter.get('/create', (req, res, next) => {
+  res.render('celebrities/create');
+});
+
 celebsRouter.get('/:id', (req, res, next) => {
   Celebrity.findById(req.params.id)
     .then((celebrity) => {
